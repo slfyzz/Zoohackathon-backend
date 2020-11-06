de15a6dfc4d78f46c4fd356e1e1330dab179e32a
slfyzz <mahmoud2571997@gmail.com>
2020-11-06 15:44:48 +0200

    provide Api for different animals and species

--- src/app.js
+++ src/app.js
@@ -21,10 +21,65 @@ app.get('/countries/all', (req, res) => {
 });
 
 
+app.get('/:country/animals', (req, res) => {
+    const country = req.params.country;
+    
+    // some querying and logic
+
+    res.send([
+        {
+            name: "Cat",
+            species: "Animals",
+            extinct: false,
+            pet: true,
+            img: ""
+        },
+        {
+            name: "Eagle",
+            species: "Birds",
+            extinct: true,
+            pet: false,
+            img: ""
+        },
+        {
+            name: "Sharks",
+            species: "Fish",
+            extinct: true,
+            pet: false,
+            img: ""
+        }, 
+        {
+            name: "Cheetah",
+            species: "Animals",
+            extinct: false,
+            pet: false,
+            img: ""
+        }
+    ]);
+});
 
 
+app.get('/animals/:animal', (req, res) => {
+    const animal = req.params.animal;
 
 
+    res.send({
+        name : animal,
+        species: animal,
+        pet: true,
+        extinct: false,
+        img: "",
+        description: "",
+        stores: [],
+        whereToSee: [],
+        comments: [],
+        whatItEats: [],
+        ageRange: "",
+        videos: []
+
+
+    });
+});
 
 
 app.listen(port, () => {
